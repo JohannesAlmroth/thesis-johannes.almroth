fd922de6ee3b63d71747d54454c288cb44007fc9
Johannes Almroth <johannes.almroth@gmail.com>
2020-01-22 14:41:28 +0100

    Fail tracker debug added

--- scripts/lib/fail_tracker.py
+++ scripts/lib/fail_tracker.py
@@ -1,12 +1,16 @@
 class Fail_tracker:
-	def init(self,
+	def __init__(self,
 	grace_period = 1,
 	cooldown = 1,
-	max_allowed_strikes = 10):
+	max_allowed_strikes = 10,
+	
+	debug = False):
 		self.GRACE_PERIOD = grace_period
 		self.COOLDOWN = cooldown
 		self.MAX_ALLOWED_STRIKES = max_allowed_strikes
 		self.strike_count = 0
+		self.DEBUG = debug
 
 	def strike(self):
-		self.strike_count += 1
+		self.strike_count = self.strike_count + 1
+		if(self.DEBUG): print("strike_count is now", self.strike_count)
